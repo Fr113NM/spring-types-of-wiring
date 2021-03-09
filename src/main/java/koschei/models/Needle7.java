f0c3972fb5d1625d133cb4a50d8e995e41564bd6
Pavel Lamov <git.habov@yandex.ru>
2021-03-10 02:25:00 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,12 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component("needle7")
+public class Needle7 {
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + "";
+    }
+}
